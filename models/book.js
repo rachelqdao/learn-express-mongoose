3f34df4d69a5d3ed259d671aaa43459c282d9a0b
Rachel Dao <rachelqdao@gmail.com>
2024-03-12 13:22:36 -0400

    fixed books, authors and genre models

--- models/book.js
+++ models/book.js
@@ -3,7 +3,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var BookSchema = new Schema(
-  {}
+  {title: {type: String, required: true},
+  author: { type: Schema.Types.ObjectId, ref: 'Author', required: true },
+  summary: {type: String, required: true},
+  ISBN: {type: String},
+  Genre: [{ type: Schema.Types.ObjectId, ref: 'Genre' }]
+  }
 );
 
 //Export model
