039b1fc1479f6ca2c053ad876f90c9d6f5e2ea93
Rachel Dao <rachelqdao@gmail.com>
2024-03-14 17:38:09 -0400

    fixing authors and book_status

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,19 @@
 let BookInstance = require('../models/bookinstance');
 
 exports.show_all_books_status = function(res) {
-  return res.send([]);
+  // get the book status using a mongoose operation
+  let book_status_list;
+  BookInstance.find({'status': {$eq: 'Available'}})
+  .populate('book')
+  .exec()
+  .then(bookInstance => {
+      // book_status_list = books
+      // console.log(book_status_list)
+      book_status_list = bookInstance.map(function(bookInstance) {
+        console.log(bookInstance.book.title + ": " + bookInstance.status)
+      })
+    }
+  )
+
+  return res.send(book_status_list);
 }
