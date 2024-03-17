b800cfc01f5aba43118c72873d4c7991ee8a0842
Rachel <rachelqdao@gmail.com>
2024-03-17 07:56:33 -0400

    fixed activities

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,19 +1,13 @@
 let BookInstance = require('../models/bookinstance');
 
 exports.show_all_books_status = function(res) {
-  // get the book status using a mongoose operation
-  let book_status_list;
   BookInstance.find({'status': {$eq: 'Available'}})
   .populate('book')
   .exec()
   .then(bookInstance => {
-      // book_status_list = books
-      // console.log(book_status_list)
-      book_status_list = bookInstance.map(function(bookInstance) {
-        console.log(bookInstance.book.title + ": " + bookInstance.status)
-      })
+      res.send(bookInstance.map(function(bookInstance) {
+        return bookInstance.book.title + ": " + bookInstance.status
+      }))
     }
   )
-
-  return res.send(book_status_list);
 }
